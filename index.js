656dbf132217b297c3e558238aa7275508d7f65f
Kevin Pham <hainamkevin.pham@gmail.com>
2020-05-27 19:45:08 +0200

    finish the boilerplate

--- index.js
+++ index.js
@@ -1,23 +1,48 @@
+const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.once("ready", () => {
     console.log("Ready!")
 })
 
+// Consider Line 7
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+
 client.on("message", message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    if (message.member.hasPermission(["ADMINISTRATOR"])) {
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-        // console.log(message.content);
+    if (!client.commands.has(commandName)) return;
+        
+    const command = client.commands.get(commandName);
 
-        if(message.content.startsWith(`${prefix}`)) {
-            // message.channel.send("Kick")
-            
-            let member = message.mentions.members.first();
-            message.channel.send(`Ich gab ${member} 'nen fetten Arschtritt.`);
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
+        if (command.usage) {
+            reply += `\nProper usage: \`${prefix}${command.name}${command.usage}\``;
         }
+        return message.channel.send(reply);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Oops, there was an error in executing this command!');
     }
 });
 
